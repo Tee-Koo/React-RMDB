16b547ed4d79335508dfef02c129e6cf0f69b7d0
Tee-Koo <tero.korpi@gmail.com>
2021-11-17 21:48:59 +0200

    Update

---
+++ src/components/Home.js
@@ -0,0 +1,20 @@
+import React, { useState } from "react"; 
+// Config
+import { POSTER_SIZE, BACKDROP_SIZE, IMAGE_BASE_URL } from "../config";
+
+// Components
+
+
+// Hook
+
+
+// Image
+import NoImage from '../images/no_image.jpg'
+
+const Home = () => {
+    const [state, setState] = useState();
+
+    
+
+    return <div>Home Page</div>
+} 
