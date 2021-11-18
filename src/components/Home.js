9a66d4872a3e02d69ab1620a20d4c31a75c9d5e8
Tee-Koo <tero.korpi@gmail.com>
2021-11-18 14:58:35 +0200

    Update

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,7 +5,8 @@ import React, { useState, useEffect } from "react";
 import { POSTER_SIZE, BACKDROP_SIZE, IMAGE_BASE_URL } from "../config";
 
 // Components
-
+import HeroImage from "./HeroImage";
+import Grid from './Grid';
 
 // Hook
 import { useHomeFetch } from '../hooks/useHomeFetch';
@@ -14,12 +15,26 @@ import { useHomeFetch } from '../hooks/useHomeFetch';
 import NoImage from '../images/no_image.jpg'
 
 const Home = () => {
-    const { state, loading, error } = useHomeFetch();
+  const { state, loading, error } = useHomeFetch();
 
+  console.log(state);
 
-    console.log(state);
-
-    return <div>Home Page</div>
+  return (
+    <>
+      {state.results[0] ? (
+        <HeroImage
+          image={`${IMAGE_BASE_URL}${BACKDROP_SIZE}${state.results[0].backdrop_path}`}
+          title={state.results[0].original_title}
+          text={state.results[0].overview}
+        />
+      ) : null}
+      <Grid header='Popular Movies'>
+        {state.results.map(movie => (
+          <div key={movie.id}>{movie.title}</div>
+        ))}
+      </Grid>
+    </>
+  );
 };
 
 export default Home;
--- src/hooks/useHomeFetch.js
+++ src/hooks/useHomeFetch.js
@@ -10,35 +10,33 @@ const initialState = {
   total_results: 0
 };
 
-
 export const useHomeFetch = () => {
-  const [state, setState] = useState();
-    const [loading, setLoading] = useState(false);
-    const [error, setError] = useState(false);
+  const [state, setState] = useState(initialState);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
 
-    const fetchMovies = async (page, searchTerm = "") => {
-        try {
-          setError(false);
-          setLoading(true);
+  const fetchMovies = async (page, searchTerm = '') => {
+    try {
+      setError(false);
+      setLoading(true);
 
-          const movies = await API.fetchMovies(searchTerm, page);
-          
+      const movies = await API.fetchMovies(searchTerm, page);
 
-          setState(prev => ({
-            ...movies,
-            results:
-              page > 1 ? [...prev.results, ...movies.results] : [...movies.results]
-          }))
-        } catch (error) {
-            setError(true);
-        }
-        setLoading(false)
-    };
+      setState(prev => ({
+        ...movies,
+        results:
+          page > 1 ? [...prev.results, ...movies.results] : [...movies.results]
+      }));
+    } catch (error) {
+      setError(true);
+    }
+    setLoading(false);
+  };
 
-    // Initial render
-    useEffect(() => {
-        fetchMovies(1)
-    }, []);
+  // Initial render
+  useEffect(() => {
+    fetchMovies(1);
+  }, []);
 
-    return { state, loading, error };
-}
+  return { state, loading, error };
+};
